2015f8fad9be367461c3c95fa447ce4cbaf61574
Anupam Shukla <118039965+ANUPAMSHUKLA-CSE@users.noreply.github.com>
2026-04-28 21:30:14 +0530

    Update Main.java

--- AbstractFactoryPattern/org.example/Main.java
+++ AbstractFactoryPattern/org.example/Main.java
@@ -2,10 +2,14 @@ package org.example;
 
 import org.example.factory.CloudServiceFactory;
 import org.example.factory.CloudServiceFactoryProvider;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String provider = args.length > 0 ? args[0] : "AWS";
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter cloud provider name (AWS, Azure, GCP): ");
+        String provider = scanner.nextLine();
 
         CloudServiceFactory factory = CloudServiceFactoryProvider.getFactory(provider);
 
